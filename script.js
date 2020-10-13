51cec0f1ac1cc34c3779e61edbdba24e89e3bd66
AlexandrBabash <more-712@mail.ru>
2020-10-13 19:26:13 +0300

    new version

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 
 // ----------- Задача №1 -------------
 
-// function sum(a, b) {
-//     if (a + b < 100) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+function sum(a, b) {
+    if (a + b < 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
    
-// console.log(sum(15, 20));
+console.log(sum(15, 20));
 
 
 // ----------- Задача №2 -------------
 
-// function convert(a, b) {
+function convert(a, b) {
 
-//     return a * 3600 + b * 60;
-// }
+    return a * 3600 + b * 60;
+}
 
-// console.log(convert(2, 20));
+console.log(convert(2, 20));
 
 
 
 // ----------- Задача №3 -------------
 
-// function total(a, b, c) {
-//     return a * 3 + b * 1 + c * 0;
-// }
+function total(a, b, c) {
+    return a * 3 + b * 1 + c * 0;
+}
 
-// console.log(total(3, 4, 4));
+console.log(total(3, 4, 4));
 
 
 
