73b1000ed4634d06e9397d58dfb08a5f3211fa19
AlexandrBabash <more-712@mail.ru>
2020-10-14 11:52:14 +0300

    corrections to new version

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 
 // ----------- Задача №1 -------------
 
-function sum(a, b) {
-    if (a + b < 100) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function sum(a, b) {
+//     if (a + b < 100) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
    
-console.log(sum(15, 20));
+// console.log(sum(15, 20));
 
 
 // ----------- Задача №2 -------------
 
-function convert(a, b) {
+// function convert(a, b) {
 
-    return a * 3600 + b * 60;
-}
+//     return a * 3600 + b * 60;
+// }
 
-console.log(convert(2, 20));
+// console.log(convert(2, 20));
 
 
 
 // ----------- Задача №3 -------------
 
-function total(a, b, c) {
-    return a * 3 + b * 1 + c * 0;
-}
+// function total(a, b, c) {
+//     return a * 3 + b * 1 + c * 0;
+// }
 
-console.log(total(3, 4, 4));
+// console.log(total(3, 4, 4));
 
 
 
 // ----------- Задача №4 -------------
 
 function makePair(a, b) {
-    array = [a, b];
-    return array;
+    return [a, b];
 }
 
 console.log(makePair(5, 10));
